dde001cbf09e1c75e1e0518f8486d86a4429b1d0
Emma Scanlon <emmascanlon4@gmail.com>
2019-03-07 16:25:50 -0700

    now players index displays

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var currentTeam = {};
+
+$(document).ready( function() {
+  $('.team-item').on('click', function() {
+    currentTeam.id = this.dataset.id
+    $.ajax({
+      url: '/teams/' + currentTeam.id + '/players',
+      method: "GET",
+      dataType: "JSON"
+    }).done( function(players) {
+      var list = $('#players');
+      list.empty();
+      players.forEach( function(play) {
+        var li = '<li data-player-id="' + play.id + '">' + play.name + ' - #'+ play.number + '</li>'
+        list.append(li)
+      });
+    });
+  });
+}); 
